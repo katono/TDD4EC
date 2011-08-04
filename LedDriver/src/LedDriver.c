197f84434c1a67ee4073d2dd1fcd6e6ea2f151a1
katono <katono123@gmail.com>
2011-08-04 22:03:35 +0900

    仮実装でとりあえず成功させる。

--- LedDriver/src/LedDriver.c
+++ LedDriver/src/LedDriver.c
@@ -1,11 +1,15 @@
 #include "LedDriver.h"
 
+static uint16_t *ledsAddress;
+
 void LedDriver_Create(uint16_t *address)
 {
+	ledsAddress = address;
 	*address = 0;
 }
 
 void LedDriver_TurnOn(int ledNumber)
 {
+	*ledsAddress = 1;
 }
 
