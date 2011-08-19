34e173fa56439e3cd16b376393263e0510c1ccc1
katono <katono123@gmail.com>
2011-08-19 15:06:54 +0900

    LedDriver_TurnOffを修正してtest_TurnOffAnyLedをパスした。

--- LedDriver/src/LedDriver.c
+++ LedDriver/src/LedDriver.c
@@ -25,7 +25,7 @@ void LedDriver_TurnOn(int ledNumber)
 
 void LedDriver_TurnOff(int ledNumber)
 {
-	*ledsAddress = 0;
+	*ledsAddress &= ~(convertLedNumberToBit(ledNumber));
 }
 
 void LedDriver_TurnAllOn(void)
